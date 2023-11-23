b1e518cb42012202d1634284552652313588ab68
Jorge Infante <jorge.infante@ssc.com.co>
2023-11-23 00:20:11 -0500

    configuracion work service

---
+++ src/service-worker.js
@@ -0,0 +1,37 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
+);
+
+if (workbox) {
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+} else {
+  console.error("Workbox could not be loaded. No offline support.");
+}
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("survey-app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app.js",
+        // Add other static assets
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event)=>{
+  console.log('AHORA ESTÁ ACTIVO EL SERVICE WORKER')
+})
+self.addEventListener("fetch", (event) => {
+  console.log('EVENT LISTERNET FETCH: ',event)
+  const resp = new Response(`offLine`)
+
+  
+  if (event.request.url.includes('https://test-apiothras.djsoftwaremakers.com')){
+    const res = fetch(event.request).catch(()=>{
+      return resp
+    })
+    event.respondWith(res)
+  }
+});
