f833acc20be9742b9d4027619dc8524c374185d1
Jorge Infante <jorge.infante@ssc.com.co>
2023-12-13 09:00:53 -0500

    SW

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import store from "./store";
 import router from "./router";
-import './assets/scss/styles.scss'
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js')
+import "./assets/scss/styles.scss";
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
 }
-
 loadFonts();
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
