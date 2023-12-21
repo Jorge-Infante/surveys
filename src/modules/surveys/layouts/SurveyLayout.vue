4fa22ab1ae03661a6bc47063124f40937c8a3fd1
Jhon Aguirre <jhon.aguirre@ssc.com.co>
2023-12-21 13:28:11 -0500

    [DEV] PUT fixed

--- src/modules/surveys/layouts/SurveyLayout.vue
+++ src/modules/surveys/layouts/SurveyLayout.vue
@@ -10,7 +10,7 @@
         src="@/assets/images/logo-o.jpg"
       ></v-img>
       </v-toolbar-title>
-      Version 1.6
+      Version 1.7
 
       <v-spacer></v-spacer>
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.6';
+const CACHE_VERSION = 'v1.7';
 
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
